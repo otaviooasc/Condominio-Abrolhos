116643018b79e202d631701cd3e1e12e6e65e68f
Otávio Cavalcanti <otaviooasc@gmail.com>
2021-01-24 00:59:18 -0300

    Rota

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { DebitsComponent } from './pages/debits/debits.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'debits', component: DebitsComponent},
+  {path: '**', redirectTo:'', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
